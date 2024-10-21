dafab4fb3d9c232efe40f429d765b27ccb855543
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-10-21 18:13:42 +0530

    changes

--- 09_error_handling/youtube_manager.py
+++ 09_error_handling/youtube_manager.py
@@ -4,7 +4,9 @@ import json
 def load_data():
     try:
         with open('youtube.txt','r') as file:
-            return json.loads(file) #load that data into json and read the data
+            test =  json.load(file) #load that data into json and read the data
+            #print(type(test)) #this is a list but a json list
+            return test
     except FileNotFoundError:
         return []
   
@@ -12,9 +14,13 @@ def save_data_helper(videos):
       with open('youtube.txt','w') as file:
            return json.dump(videos,file)
 
-def list_all_videos(videos):
+def list_all_videos(videos): #we need to provide indexing here
+    print('\n')
+    print('*' * 70)
     for index, video in enumerate(videos,start=1):
-        print(f"{index}. ")
+        print(f"{index}. {video['name']}, Duration : {video['time']}")
+    print('\n')
+    print('*' * 70)
 
 def add_videos(videos):
     name = input("Enter video name: ")
@@ -24,10 +30,24 @@ def add_videos(videos):
     
     
 def update_videos(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input("Enter the list number you want to delete: "))
+    if   1 <= index <= len(videos):
+        name = input("Enter the new video name: ")
+        time = input("Enter the new video time")
+        videos[index-1] = {'name':name, 'time': time}
+        save_data_helper(videos)
+    else:
+        print("Invalid Index selected")
 
 def delete_videos(videos):
-    pass
+    list_all_videos(videos)
+    index = int(input('Enter the index you want to delete'))
+    if   1 <= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("Invalid video index selected")
 
 
 
@@ -42,7 +62,7 @@ def main():
         print('5. Exit the app')
 
         choice = input('Enter your choice ')
-        print(videos)
+        #print(videos)
 
         if choice == '1':
             list_all_videos(videos)
