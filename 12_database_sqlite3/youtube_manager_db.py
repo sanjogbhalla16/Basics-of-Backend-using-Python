169df3f28ec0400307699506c10535d1d2c3004e
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2024-10-22 17:33:00 +0530

    changes

--- 12_database_sqlite3/youtube_manager_db.py
+++ 12_database_sqlite3/youtube_manager_db.py
@@ -2,10 +2,10 @@ import sqlite3
 
 conn = sqlite3.connect('youtube_videos.db')
 
-cursor = conn.cursor
+cursor = conn.cursor()
 
 cursor.execute('''
-               CREATE TABLE IF NOT EXIST videos(
+               CREATE TABLE IF NOT EXISTS videos(
                    id INTEGER PRIMARY KEY,
                    name TEXT NOT NULL,
                    time TEXT NOT NULL
@@ -13,17 +13,26 @@ cursor.execute('''
 ''')
 
 def list_all_videos():
-    pass
+    conn.execute("SELECT * FROM  videos")
+    rows = cursor.fetchall()
+    if not rows:
+        print("No videos found in the database.")
+    else:
+        for row in rows:
+            print(row)
 
-def add_videos():
-    pass 
+def add_videos(name,time):
+    cursor.execute("INSERT INTO videos (name,time) VALUES (?, ?)", (name,time))
+    conn.commit()
 
 
-def update_videos():
-    pass
+def update_videos(video_id, new_name,new_time):
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE  id = ?",(new_name,new_time,video_id))
+    conn.commit()
 
-def delete_videos():
-    pass
+def delete_videos(video_id):
+    cursor.execute("DELETE FROM videos WHERE id = ?",(video_id,))
+    conn.commit()
 
 def main():
     while True:
@@ -56,7 +65,8 @@ def main():
             break
         else:
             print('Invalid Choice')
-
+    
+    conn.close()
 
 if __name__ == '__main__':
     main()
